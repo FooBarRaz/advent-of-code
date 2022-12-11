7d984623ef49fa9979515d4b6d14e8e87ef6884d
raz <mehraz.hakim@gmail.com>
2022-12-11 18:17:37 -0500

    --wip-- [skip ci]

---
+++ src/2022/day10/problemSolver.test.ts
@@ -0,0 +1,171 @@
+import {parseInput, problem1} from "./problemSolver";
+
+const smallTestInput = `noop
+addx 3
+addx -5`;
+
+const largeTestInput = `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`
+describe('day 10', function () {
+    describe('parse input', function () {
+        it('it should parse noop', function () {
+            const result = parseInput(smallTestInput)
+            expect(result[0]).toEqual({ type: 'noop' })
+            expect(result[1]).toEqual({ type: 'addx', value: 3 })
+            expect(result[2]).toEqual({ type: 'addx', value: -5 })
+        });
+    });
+
+    describe('problem1', function () {
+        it('should ', function () {
+            const result  = problem1(largeTestInput)
+            expect(result).toEqual(0)
+        });
+    });
+});
+
+
---
+++ src/2022/day10/problemSolver.ts
@@ -0,0 +1,60 @@
+type NoOp = {
+    type: 'noop'
+}
+type AddX = {
+    type: 'addx'
+    value: number
+}
+
+export type Instructions = NoOp | AddX
+
+export const crt = (instructions: Instructions[]) => {
+    let currentRegister = 1;
+    let valuesToAdd = Array(instructions.length).fill(null)
+    let registerValues = Array(instructions.length).fill(null)
+
+    return {
+        processInstruction(instruction: Instructions, index: number) {
+            if(instruction.type === 'addx') {
+                valuesToAdd[index+2] = instruction.value
+            }
+        },
+        finishCycle(index: number) {
+            const newValue = index === 0 ? 1
+                : (valuesToAdd[index] || 0) + currentRegister
+
+            currentRegister = newValue
+            registerValues[index] = newValue
+        },
+        run() {
+            instructions.forEach((instruction, i) => {
+                this.processInstruction(instruction, i)
+                this.finishCycle(i)
+            })
+        },
+    }
+}
+
+
+export const parseInput = (input: string): Instructions[] => {
+    return input.split('\n')
+        .map(line => {
+            const [type, value] = line.split(' ')
+            if (type === 'noop') {
+                return {type: 'noop'}
+            }
+            if (type === 'addx') {
+                return {type: 'addx', value: parseInt(value)}
+            }
+        })
+}
+
+export const problem1 = (input: string): number => {
+    // measurements to check:
+    // 20th, 60th, 100th, 140th, 180th, and 220th
+    const measurements = [19, 59, 99, 139, 179, 219]
+    const instructions = parseInput(input)
+    const processor = crt(instructions)
+
+    return 0
+}
