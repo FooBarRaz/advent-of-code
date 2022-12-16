afb00403e6c02e4df1c7bef74ed9566de2d1c8fa
Raz Ali <mehraz.hakim@gmail.com>
2022-12-16 13:41:20 -0500

    abandoning day13

--- src/2022/day13/problemSolver.test.ts
+++ src/2022/day13/problemSolver.test.ts
@@ -1,4 +1,4 @@
-import {parseInput} from "./problemSolver";
+import {ArrayOfNumbers, comparePair, parseInput} from "./problemSolver";
 
 const testInput = `[1,1,3,1,1]
 [1,1,5,1,1]
@@ -34,6 +34,87 @@ describe('day 13', function () {
         });
     });
     describe('given test input', function () {
+        describe('compare', function () {
+            describe('given two integers', function () {
+                const parsedInput = parseInput(testInput);
+                const testCases:
+                    {
+                        id?: string,
+                        left: number | ArrayOfNumbers,
+                        right: number | ArrayOfNumbers,
+                        expectedToBeOrdered: 1 | 0 | -1
+                    }[] = [
+                    {
+                        id: 'ints',
+                        left: 1,
+                        right: 2,
+                        expectedToBeOrdered: 1
+                    },
+                    {
+                        id: 'ints',
+                        left: 1,
+                        right: 1,
+                        expectedToBeOrdered: 0
+                    },
+                    {
+                        left: 3,
+                        right: 2,
+                        expectedToBeOrdered: -1
+                    },
+                    {
+                        left: [1],
+                        right: [2],
+                        expectedToBeOrdered: 1
+                    },
+                    {
+                        left: [3],
+                        right: [2],
+                        expectedToBeOrdered: -1
+                    },
+                    {
+                        id: 'pair1',
+                        left: parsedInput[0][0],
+                        right: parsedInput[0][1],
+                        expectedToBeOrdered: 1
+                    },
+                    {
+                        id: 'pair2',
+                        left: parsedInput[1][0],
+                        right: parsedInput[1][1],
+                        expectedToBeOrdered: 1
+                    },
+                    {
+                        id: 'pair3',
+                        left: parsedInput[2][0],
+                        right: parsedInput[2][1],
+                        expectedToBeOrdered: -1
+                    },
+                    {
+                        id: 'pair4',
+                        left: parsedInput[3][0],
+                        right: parsedInput[3][1],
+                        expectedToBeOrdered: 1
+                    },
+                    {
+                        id: 'pair5',
+                        left: parsedInput[4][0],
+                        right: parsedInput[4][1],
+                        expectedToBeOrdered: -1
+                    },
+
+                ]
+
+                testCases
+                    // .filter(x => x.id === 'pair2')
+                    .forEach(testCase => {
+                    const {id, left, right, expectedToBeOrdered} = testCase;
+                    it(`${id} should compare pair ${left}, ${right} side by side`, function () {
+                        const result = comparePair(left, right)
+                        expect(result).toEqual(expectedToBeOrdered)
+                    });
+                })
+            });
+        });
     });
 
 });
--- src/2022/day13/problemSolver.ts
+++ src/2022/day13/problemSolver.ts
@@ -1,8 +1,64 @@
-type ArrayOfNumbers = number[] | Array<ArrayOfNumbers>
+import {groupBy, isEqual, isNumber} from "lodash";
+
+export type ArrayOfNumbers = number[] | Array<ArrayOfNumbers>
 export const parseInput = (input: string): ArrayOfNumbers => {
     const pairs = input.split('\n\n')
     return pairs.map(pair => {
         const lines = pair.split('\n')
         return lines.map(line => eval(line))
     })
+}
+
+
+function compareArray(left: ArrayOfNumbers, right: ArrayOfNumbers): -1 | 0 | 1 {
+    if (isEqual(left, right)) return 0;
+    let itemsInLeft = left.length;
+    while(itemsInLeft--) {
+        if (right[itemsInLeft]) {
+            const comparison = comparePair(left[itemsInLeft], right[itemsInLeft])
+            if (comparison === -1) {
+                return -1
+            }
+        }
+        return -1
+    }
+    if (left.length < right.length) {
+        return 1
+    }
+    // return left.every((leftItem, index) => {
+    //     if (right[index]) {
+    //         return comparePair(leftItem, right[index])
+    //     }
+    //     else return -1
+    //     // return right[index] && comparePair(leftItem, right[index]) >= 0;
+    // }) ? 1 : -1
+}
+
+export const comparePair = (left: number | ArrayOfNumbers, right: number | ArrayOfNumbers): -1 | 0 | 1 => {
+    if (isNumber(left) && isNumber(right)) {
+        if(left < right) {
+            return 1
+        }
+        else if (right < left) {
+            return -1
+        }
+        return 0
+    }
+    if (Array.isArray(left) && Array.isArray(right)) {
+        return compareArray(left, right)
+    }
+    const intergerValues = [left, right].filter(val => isNumber(val));
+    if(intergerValues.length === 1) {
+
+       const [newLeft, newRight] =  [left, right].map(val => {
+            if(isNumber(val)) {
+                return [val]
+            }
+            else return val
+        })
+
+        return compareArray(newLeft, newRight)
+    }
+    throw new Error('wtf')
+
 }
